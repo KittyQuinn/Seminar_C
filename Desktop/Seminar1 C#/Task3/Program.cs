4ac886e40d71802d51171f8dfc333073fb394092
Катя Рыжова <kataryzova@Air-Kata.lan>
2022-09-02 18:53:57 +0200

    Added task3

---
+++ Desktop/Seminar1 C#/Task3/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.Clear();
+Console.Write("Write number ");
+int number = int.Parse(Console.ReadLine());
+
+int i = number;
+
+if (number < 0)
+{
+    number = number * -1;
+}
+else if (number > 0)
+{
+    i = i * -1;
+}
+else 
+{
+    System.Console.WriteLine("0");
+}
+
+while (i < number)
+{
+    System.Console.WriteLine(i);
+    i++;
+}
+
+System.Console.WriteLine(number);
