fc63d26b1e696f733648d2b31a1a5310bb93fc88
Катя Рыжова <kataryzova@Air-Kata.lan>
2022-09-02 18:26:17 +0200

    Added task1

---
+++ Desktop/Seminar1 C#/Task1/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+Console.Write("Write number1 ");
+int number1 = int.Parse(Console.ReadLine());
+Console.Write("Write number2 ");
+int number2 = int.Parse(Console.ReadLine());
+
+if(number2 == number1 * number1)
+{
+    Console.WriteLine("Yes");
+}
+else 
+{
+    Console.WriteLine("No");
+}
