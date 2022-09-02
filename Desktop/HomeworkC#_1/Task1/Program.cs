b0c01384b6feb3597518d067ae9050425034210a
Катя Рыжова <kataryzova@Air-Kata.lan>
2022-09-02 23:20:42 +0200

    Added 1st task

---
+++ Desktop/HomeworkC#_1/Task1/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+Console.Write("Write number1 ");
+int number1 = int.Parse(Console.ReadLine());
+Console.Write("Write number2 ");
+int number2 = int.Parse(Console.ReadLine());
+
+if (number1 > number2)
+{
+    Console.WriteLine($"max = {number1}");
+}
+else 
+{
+    Console.WriteLine($"max = {number2}");
+}
